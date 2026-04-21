93dc8f624eb590a3c825e6d2be8d35960e4a9272
Lucas <santoslucas347@gmail.com>
2026-04-21 20:32:23 -0300

    Parts movement

--- src/main.c
+++ src/main.c
@@ -1,28 +1,33 @@
+#define _DEFAULT_SOURCE
 #define _POSIX_C_SOURCE 199309L
 #include <stdlib.h>
 #include <ncurses.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <math.h>
+#include <stdbool.h>
 
-#define YOFFSET         5
+#define Y_PADDING       5
 #define PERIOD_MS      16
-#define PLAYER_SPEED     .3
-#define MAX_SIZE       10
-
-WINDOW *gameWindow;
-WINDOW *CreateWindow(int width, int height, int ix, int iy);
+#define PLAYER_SPEED     .1
+#define MAX_PARTS      256
 
 typedef struct v2{
 	float x;
 	float y;
 }v2;
 
+WINDOW *CreateWindow(int width, int height, int ix, int iy);
 
+v2 parts[MAX_PARTS];
+v2 partsBuffer[MAX_PARTS];
+int currentLenght = 10;
 const char *gameMsg = "Welcome to Snake Sheet! Press 'e' to quit";
 
 int main(int argc, char **argv){
 	int width, height, startx,starty = 0;
+	WINDOW *gameWindow;
 
 	initscr();
 	noecho();
@@ -33,23 +38,52 @@ int main(int argc, char **argv){
 	mvprintw(0, (width-strlen(gameMsg))/2,gameMsg);
 	attroff(A_BOLD);
 	refresh();
+
 	startx  = (width - width/2)/2;
-	height /= 2;
+	height /= 1.5;
 	width  /= 2;
-	gameWindow = CreateWindow(width,height,startx,starty+YOFFSET);
+	gameWindow = CreateWindow(width,height,startx,starty+Y_PADDING);
+
+	v2 playerPos = {width/2,(starty+height/1.5)};
+	for(int i =0; i < MAX_PARTS; ++i){
+		parts[i]    = playerPos;
+		parts[i].x -= i;
+	}
+   memcpy( partsBuffer, parts, sizeof(parts));
 	
-	v2 playerPos = {width/2,(starty+height/2)};
-	v2 playerDir = {1,0};
+	v2 playerDir = {0,0};
+	float rotation = 0;
 	timeout(PERIOD_MS);
 	struct timespec start,stop;
+
 	for(;;){
 		clock_gettime(CLOCK_MONOTONIC,&start);
-		int input = getch();
-		playerPos.x += PLAYER_SPEED;
-		if(input == 'e') break;
+		int input       = getch();
+		bool shouldMove = false;
+		if(input        == 'e') break;
+		if(input        == 'l') rotation +=(float)(M_PI/2);   
+		if(input        == 'h') rotation -=(float)(M_PI/2);
+		playerDir        = (v2) {cosf(rotation), sinf(rotation)};
+		//playerPos.x += playerDir.x * PLAYER_SPEED;
+		//playerPos.y += playerDir.y * PLAYER_SPEED;
 		werase(gameWindow);
 		box(gameWindow,0,0);
-		mvwaddch(gameWindow,playerPos.y,playerPos.x,'@');
+		//mvwaddch(gameWindow,playerPos.y,playerPos.x,'@');
+		parts[0].x += playerDir.x * PLAYER_SPEED;
+		parts[0].y += playerDir.y * PLAYER_SPEED;
+	 	//mvwaddch(gameWindow,parts[0].y,parts[0].x,'@');
+
+		if((int)(partsBuffer[0].x) != (int)(parts[0].x) || (int)(partsBuffer[0].y) != (int)(parts[0].y)){
+			for (int i = 1; i < currentLenght; ++i){
+				parts[i].x = partsBuffer[i-1].x;
+				parts[i].y = partsBuffer[i-1].y;
+			}
+			memcpy( partsBuffer, parts, sizeof(parts));
+		}
+		for(int i =0; i < currentLenght; ++i) {
+			
+			mvwaddch(gameWindow,parts[i].y,parts[i].x,'@');
+		}
 		wrefresh(gameWindow);
 
 		clock_gettime(CLOCK_MONOTONIC,&stop);
