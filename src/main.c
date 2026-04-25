13f89cd2798dea9c8295c8b786883390a74706b7
Lucas <santoslucas347@gmail.com>
2026-04-25 13:48:03 -0300

    Some code refactoring

--- src/main.c
+++ src/main.c
@@ -9,56 +9,47 @@
 #include <stdbool.h>
 #include <assert.h>
 
-#define Y_PADDING       5
-#define PERIOD_MS      16
-#define PLAYER_SPEED     .1
-#define SPEED_BOOST      .5
-#define MAX_PARTS      256
-#define GREEN_RGB      78,521,149
+#define TOP_CROP_SIZE         5
+#define FRAME_PERIOD_MS      16
+#define PLAYER_SPEED          .1
+#define SPEED_BOOST           .5
+#define MAX_PARTS           256
+#define GREEN_RGB           78,521,149
 
 typedef struct v2{
 	float x;
 	float y;
 }v2;
 
+typedef enum state{
+	GAMEPLAY   = 0,
+	GAME_OVER  = 1
+}state;
+
 WINDOW *CreateWindow(int width, int height, int ix, int iy);
 v2 SpawnPowerUp(int xConstraint, int yConstraint);
-bool GameIsOver();
+void InitCurses();
+bool IsGameOver();
 
 v2 parts[MAX_PARTS];
 v2 partsBuffer[MAX_PARTS];
 int currentLenght = 3;
+int width, height, startx,starty = 0;
 const char *gameMsg = "Welcome to Snake Sheet! Press 'e' to quit";
 
 int main(int argc, char **argv){
-	int width, height, startx,starty = 0;
 	bool canRotate = true;
 	WINDOW *gameWindow;
 	v2 playerDir   = {1,0};
 	v2 powerUpPos  = {0};
-	//float rotation = 0;
 	struct timespec start,stop;
 
-	initscr();
-	assert(has_colors());
-	start_color(); 
-	noecho();
-	init_color(COLOR_BLACK,0,0,0);
-	init_color(COLOR_GREEN,GREEN_RGB);
-	init_pair(1,COLOR_GREEN, COLOR_BLACK);
-	raw();
-	curs_set(0);
-	getmaxyx(stdscr,height,width);
-	attron(A_BOLD | COLOR_PAIR(1));
-	mvprintw(0, (width-strlen(gameMsg))/2,gameMsg);
-	attroff(A_BOLD);
-
-	refresh();
+	InitCurses();
 
 	startx  = (width - width/2)/2;
 	height /= 1.5;
 	width  /= 2;
-	gameWindow = CreateWindow(width,height,startx,starty+Y_PADDING);
+	gameWindow = CreateWindow(width,height,startx,starty+TOP_CROP_SIZE);
 	wattron(gameWindow,COLOR_PAIR(1));
 	v2 playerPos = {width/2,(starty+height/1.5)};
 	for(int i =0; i < MAX_PARTS; ++i){
@@ -68,27 +59,24 @@ int main(int argc, char **argv){
 	powerUpPos = SpawnPowerUp(width-2,height-2);
    memcpy( partsBuffer, parts, sizeof(parts));
 	
-	timeout(PERIOD_MS);
+	timeout(FRAME_PERIOD_MS);
 
 	for(;;){
 		clock_gettime(CLOCK_MONOTONIC,&start);
 		int input       = getch();
 		if(input        == 'e') break;
-		if(GameIsOver()) break;
+		if(IsGameOver()) break;
 		if((input == 'w' || input == 's') && canRotate && !(int)(playerDir.y)) {
-			//rotation +=(float)(M_PI/2); 
 			canRotate = false;
 			playerDir.y = (input == 'w') * -1 + (input == 's') * 1;
 			playerDir.x = 0;
 		}
 		if((input == 'a' || input == 'd') && canRotate  && !(int)(playerDir.x)) {
-			//rotation -=(float)(M_PI/2);
 			canRotate = false;
 			playerDir.x = (input == 'a') * -1 + (input == 'd') * 1;
 			playerDir.y = 0;
 		}
 
-		//playerDir        = (v2) {cosf(rotation), sinf(rotation)};
 		werase(gameWindow);
 		box(gameWindow,0,0);
 
@@ -122,7 +110,7 @@ int main(int argc, char **argv){
 
 		clock_gettime(CLOCK_MONOTONIC,&stop);
 		double elapsed   = (stop.tv_sec - start.tv_sec) + (stop.tv_nsec - start.tv_nsec)/1e9;
-		double remaining = (double)(PERIOD_MS/1e3) - elapsed;
+		double remaining = (double)(FRAME_PERIOD_MS/1e3) - elapsed;
 		if (remaining > 0) sleep(remaining);
 		
 	}	
@@ -161,7 +149,7 @@ v2 SpawnPowerUp(int xConstraint, int yConstraint){
 	
 }
 
-bool GameIsOver(){
+bool IsGameOver(){
 	for(int i = 1; i < currentLenght; ++i){
 		if((int)(parts[0].x) == (int)(parts[i].x) && (int)(parts[0].y) == (int)(parts[i].y)) return true; 
 	}
@@ -178,7 +166,23 @@ void UpdateAndDrawGameOver(){
 
 }
 
+void InitCurses(){
+	initscr();
+	assert(has_colors());
+	start_color(); 
+	noecho();
+	init_color(COLOR_BLACK,0,0,0);
+	init_color(COLOR_GREEN,GREEN_RGB);
+	init_pair(1,COLOR_GREEN, COLOR_BLACK);
+	raw();
+	curs_set(0);
+	getmaxyx(stdscr,height,width);
+	attron(A_BOLD | COLOR_PAIR(1));
+	mvprintw(0, (width-strlen(gameMsg))/2,gameMsg);
+	attroff(A_BOLD);
 
+	refresh();
+}
 
 
 
