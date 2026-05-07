493a9864196fd65d31796ad75f66ea831647bb52
Lucas <santoslucas347@gmail.com>
2026-05-07 16:25:27 -0300

    Tetris: Fixed error when printing empty spaces in blocks and added a game window

--- src/tetris.c
+++ src/tetris.c
@@ -1,11 +1,15 @@
 #define _DEFAULT_SOURCE
-#define BLOCKS_NUM      8 //8 block forms: GENERIC, O, I, S, Z, L, J, T
-#define BLOCK_WIDTH     3
-#define BLOCK_HEIGHT    4
-#define FRAME_PERIOD_MS 16
+#define BLOCKS_NUM       8 //8 block forms: GENERIC, O, I, S, Z, L, J, T
+#define BLOCK_WIDTH      3
+#define BLOCK_HEIGHT     4
+#define BLOCK_SPEED      2
+#define FRAME_PERIOD_MS  16
+#define GAME_WIDTH       20
+#define GAME_HEIGHT      20
 #define BLOCK_MEM_SIZE ((BLOCK_WIDTH+1) * BLOCK_HEIGHT) * 4 //e.g. 4x3 +1 right space, 4 frames
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <ncurses.h>
 #include <locale.h>
 #include <unistd.h>
@@ -44,8 +48,8 @@ char blockGraphics[BLOCKS_NUM][BLOCK_MEM_SIZE] = {
 	}, 
 
 	{ /* O */
-		"### ### ### ###\n"
-		"### ### ### ###\n"
+		"##X ### ### ###\n"
+		"##X ### ### ###\n"
 		"XXX XXX XXX XXX\n"
 		"XXX XXX XXX XXX\n"
 	},
@@ -79,10 +83,10 @@ char blockGraphics[BLOCKS_NUM][BLOCK_MEM_SIZE] = {
 	},
 
 	{ /* J */
-		"### XXX XXX XXX\n"
-		"### XXX XXX XXX\n"
-		"### XXX XXX XXX\n"
-		"### XXX XXX XXX\n"
+		"X#X XXX XXX XXX\n"
+		"X#X XXX XXX XXX\n"
+		"##X XXX XXX XXX\n"
+		"XXX XXX XXX XXX\n"
 	},
 
 	{ /* T */
@@ -93,46 +97,72 @@ char blockGraphics[BLOCKS_NUM][BLOCK_MEM_SIZE] = {
 	}
 };
 
+void CloseWindows();
 
-int main(){
+
+WINDOW *gameWindow;
+
+int main()
+{
+	int terminalWidth  = 0;
+	int terminalHeight = 0;
+
 	setlocale(LC_ALL,"");
 	initscr();
 	noecho();
 	raw();
 	curs_set(0);
 	move(0,0);
+
+	getmaxyx(stdscr, terminalHeight, terminalWidth);
 	
+	if(terminalWidth < GAME_WIDTH +2 || terminalHeight < GAME_HEIGHT + 5){
+		printw("Terminal too small...\nPress any key to continue\n");
+		getch();
+		CloseWindows();
+		return 1;
+	}
+	
+	gameWindow = newwin(GAME_HEIGHT,GAME_WIDTH,terminalHeight-GAME_HEIGHT,(terminalWidth-GAME_WIDTH)*.5);
 	block newBlock = {0};
-	newBlock.type = J;
-	
+	newBlock.type = O;
+	box(gameWindow,0,0);
+	wrefresh(gameWindow);
 	timeout(FRAME_PERIOD_MS);
-	float yCursor = 0;
-	float xCursor = 0;
+	float yCursor = 1;
+	float xCursor = 1;
 	double delta  = FRAME_PERIOD_MS/1e3;
 	for(;;){
 		clock_gettime(CLOCK_MONOTONIC,&start);
 		int input = getch();   	
 		if(input == 'e') break;
-		erase();
-		move(yCursor,xCursor);
+		werase(gameWindow);
+		wmove(gameWindow,yCursor,xCursor);
+		box(gameWindow,0,0);
+
 		for(int row = 0; row < 4; row++){
 			for(int column = 0; column < 4; ++column){
 				char point = blockGraphics[newBlock.type][row*16 + column];
-				if(point == '#') printw("%s","▓");
-				if(point == ' ') move(yCursor + 1 ,xCursor);
-				if(point == 'X') move(getcury(stdscr),xCursor+1);
+				if(point == '#') wprintw(gameWindow,"%s","▓");
+				if(point == ' ') wmove(gameWindow,yCursor + 1 ,xCursor);
+				if(point == 'X') wmove(gameWindow,getcury(gameWindow),getcurx(gameWindow) + 1);
 			}
 		}
-		yCursor += .5 * delta;
-		xCursor  = 0;
+		yCursor += BLOCK_SPEED* delta;
+		xCursor  = 1;
+
+		wrefresh(gameWindow);
 		clock_gettime(CLOCK_MONOTONIC,&stop);
 		delta = (stop.tv_sec - start.tv_sec) + (stop.tv_nsec - start.tv_nsec)/1e9;
 		if(delta*1e3 < FRAME_PERIOD_MS) sleep(FRAME_PERIOD_MS/1e3 - delta);
 	}
-
-	endwin();
-
-
+	
+	CloseWindows();
 	return 0;
 
+}
+
+void CloseWindows(){
+	endwin();
+	delwin(gameWindow);
 }
