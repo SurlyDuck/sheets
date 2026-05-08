24699f031cda8ae4d87cc6f58c31ff9598bbffaf
Lucas <santoslucas347@gmail.com>
2026-05-08 20:18:01 -0300

    Block movement

--- src/tetris.c
+++ src/tetris.c
@@ -1,4 +1,11 @@
 #define _DEFAULT_SOURCE
+#include <stdlib.h>
+#include <locale.h>
+#include <unistd.h>
+#include <time.h>
+
+#include <ncurses.h>
+
 #define BLOCKS_NUM       8 //8 block forms: GENERIC, O, I, S, Z, L, J, T
 #define BLOCK_WIDTH      3
 #define BLOCK_HEIGHT     4
@@ -8,14 +15,6 @@
 #define GAME_HEIGHT      20
 #define BLOCK_MEM_SIZE ((BLOCK_WIDTH+1) * BLOCK_HEIGHT) * 4 //e.g. 4x3 +1 right space, 4 frames
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <ncurses.h>
-#include <locale.h>
-#include <unistd.h>
-#include <time.h>
-
-struct timespec start, stop;
 
 typedef enum blockType{
 	GENERIC,
@@ -41,59 +40,59 @@ typedef struct block{
 //TODO: \n can be replaced by empty space
 char blockGraphics[BLOCKS_NUM][BLOCK_MEM_SIZE] = {
 	{ /* GENERIC */
-		"#XX XXX XXX XXX\n"
-		"X#X XXX XXX XXX\n"
-		"XX# XXX XXX XXX\n"
-		"XXX XXX XXX XXX\n"
+		"#XX XXX XXX XXX "
+		"X#X XXX XXX XXX "
+		"XX# XXX XXX XXX "
+		"XXX XXX XXX XXX "
 	}, 
 
 	{ /* O */
-		"##X ### ### ###\n"
-		"##X ### ### ###\n"
-		"XXX XXX XXX XXX\n"
-		"XXX XXX XXX XXX\n"
+		"##X ##X ##X ##X "
+		"##X ##X ##X ##X "
+		"XXX XXX XXX XXX "
+		"XXX XXX XXX XXX "
 	},
 
 	{ /* I */
-		"#XX ### #XX ###\n"
-		"#XX XXX #XX XXX\n"
-		"#XX XXX #XX XXX\n"
-		"#XX XXX #XX XXX\n"
+		"#XX ### #XX ### "
+		"#XX XXX #XX XXX "
+		"#XX XXX #XX XXX "
+		"#XX XXX #XX XXX "
 	},
 
 	{ /* S */
-		"X## XXX XXX XXX\n"
-		"##X XXX XXX XXX\n"
-		"XXX XXX XXX XXX\n"
-		"XXX XXX XXX XXX\n"
+		"X## X#X X## X#X "
+		"##X X## ##X X## "
+		"XXX XX# XXX XX# "
+		"XXX XXX XXX XXX "
 	},
 
 	{ /* Z */
-		"##X XXX XXX XXX\n"
-		"X## XXX XXX XXX\n"
-		"XXX XXX XXX XXX\n"
-		"XXX XXX XXX XXX\n"
+		"##X XXX XXX XXX "
+		"X## XXX XXX XXX "
+		"XXX XXX XXX XXX "
+		"XXX XXX XXX XXX "
 	},
 
 	{ /* L */
-		"#XX XXX XXX XXX\n"
-		"#XX XXX XXX XXX\n"
-		"##X XXX XXX XXX\n"
-		"XXX XXX XXX XXX\n"
+		"#XX XXX XXX XXX "
+		"#XX XXX XXX XXX "
+		"##X XXX XXX XXX "
+		"XXX XXX XXX XXX "
 	},
 
 	{ /* J */
-		"X#X XXX XXX XXX\n"
-		"X#X XXX XXX XXX\n"
-		"##X XXX XXX XXX\n"
-		"XXX XXX XXX XXX\n"
+		"X#X XXX XXX XXX "
+		"X#X XXX XXX XXX "
+		"##X XXX XXX XXX "
+		"XXX XXX XXX XXX "
 	},
 
 	{ /* T */
-		"### XXX XXX XXX\n"
-		"X#X XXX XXX XXX\n"
-		"XXX XXX XXX XXX\n"
-		"XXX XXX XXX XXX\n"
+		"### XX# X#X #XX "
+		"X#X X## ### ##X "
+		"XXX XX# XXX #XX "
+		"XXX XXX XXX XXX "
 	}
 };
 
@@ -106,6 +105,8 @@ int main()
 {
 	int terminalWidth  = 0;
 	int terminalHeight = 0;
+	float yCursor = 1;
+	float xCursor = GAME_WIDTH/2-1;
 
 	setlocale(LC_ALL,"");
 	initscr();
@@ -125,24 +126,32 @@ int main()
 	
 	gameWindow = newwin(GAME_HEIGHT,GAME_WIDTH,terminalHeight-GAME_HEIGHT,(terminalWidth-GAME_WIDTH)*.5);
 	block newBlock = {0};
-	newBlock.type = T;
+	newBlock.type = S;
 	box(gameWindow,0,0);
 	wrefresh(gameWindow);
 	timeout(FRAME_PERIOD_MS);
-	float yCursor = 1;
-	float xCursor = 1;
 	double delta  = FRAME_PERIOD_MS/1e3;
+	struct timespec start, stop;
 	for(;;){
 		clock_gettime(CLOCK_MONOTONIC,&start);
 		int input = getch();   	
-		if(input == 'e') break;
+		if(input == 'e' || input == 'E') break;
+		if(input == 'r' || input == 'R'){
+			++newBlock.rotation;
+
+			if(newBlock.rotation >= 4) newBlock.rotation = 0;
+		}else if(input == 'l' || input == 'L'){
+			++xCursor;
+		}else if(input == 'h' || input == 'H'){
+			--xCursor;
+		}
 		werase(gameWindow);
 		wmove(gameWindow,yCursor,xCursor);
 		box(gameWindow,0,0);
 		
 		float currentY = yCursor;
 		for(int row = 0; row < 4; row++){
-			for(int column = 0; column < 4; ++column){
+			for(int column = 0 + newBlock.rotation * 4; column < 4 + newBlock.rotation*4; ++column){
 				char point = blockGraphics[newBlock.type][row*16 + column];
 				if(point == '#') wprintw(gameWindow,"%s","▓");
 				if(point == ' ') wmove(gameWindow,++currentY,xCursor);
@@ -150,7 +159,7 @@ int main()
 			}
 		}
 		yCursor += BLOCK_SPEED* delta;
-		xCursor  = 1;
+		//xCursor  = 1;
 
 		wrefresh(gameWindow);
 		clock_gettime(CLOCK_MONOTONIC,&stop);
